e3fc9560772e300873f995d4df3bccb964b095ae
xiongye77 <36766101+xiongye77@users.noreply.github.com>
2020-01-11 10:35:26 +1100

    Update elb-web.tf

--- terraform/elb-web.tf
+++ terraform/elb-web.tf
@@ -19,7 +19,7 @@ resource "aws_elb" "web-elb" {
   cross_zone_load_balancing = true
   connection_draining = true
   connection_draining_timeout = 400
-  tags {
+  tags = {
     Name = "web-elb"
   }
 }
